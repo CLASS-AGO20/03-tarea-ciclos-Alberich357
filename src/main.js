fecde4a531e72102292065dcc4dd6499f721a819
Alberich LLerenas <allerenas4@ucol.mx>
2020-12-11 22:16:45 -0600

    implemented sumatoria serie dos

--- src/main.js
+++ src/main.js
@@ -6,10 +6,25 @@ export default class App {
         }
         return suma;
         }
-}
+        sumatoriaSerieDos(numero) {
+            let suma = 1;
+            for (let i = 2; i <= numero; i++) {
+                if (i % 2 === 0) {
+                suma = suma + 1 / i;
+                } else {
+                suma = suma - 1 / i;
+                }
+            }
+            return suma;
+            }
+    }
 
 let app = new App();
 console.log("Funcion sumatoria:");
 console.log(app.sumatoriaSerieUno(5));
 console.log(app.sumatoriaSerieUno(10));
 console.log(app.sumatoriaSerieUno(15));
+console.log("Funcion sumatoria de la serie dos:");
+console.log(app.sumatoriaSerieDos(5));
+console.log(app.sumatoriaSerieDos(10));
+console.log(app.sumatoriaSerieDos(15));
