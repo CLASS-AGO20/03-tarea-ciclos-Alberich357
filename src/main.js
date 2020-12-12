95c09707cb642e001699dff7e22072e0bacb899f
Alberich LLerenas <allerenas4@ucol.mx>
2020-12-11 22:08:50 -0600

    implemented sumatoria

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-export default class App {}
+export default class App {
+    sumatoriaSerieUno(numero) {
+        let suma = 1;
+        for (let i = 2; i <= numero; i++) {
+            suma = suma + 1 / i;
+        }
+        return suma;
+        }
+}
 
 let app = new App();
+console.log("Funcion sumatoria:");
+console.log(app.sumatoriaSerieUno(5));
+console.log(app.sumatoriaSerieUno(10));
+console.log(app.sumatoriaSerieUno(15));
